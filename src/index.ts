486489289fe85d3c4208b610d2481ebb12bc2ead
ikonan <2321514568@qq.com>
2025-08-04 15:50:36 +0800

    refactor: 移除 lodash 依赖，简化配置返回结构

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,53 @@
-import { merge } from "lodash";
 export { pathsToModuleNameMapper } from "ts-jest";
 
 export default (options: any = {}) => {
-  return merge(
-    {
-      verbose: true,
-      clearMocks: true,
-      collectCoverage: true,
-      coverageDirectory: ".coverage",
-      coverageProvider: "v8",
-      preset: "ts-jest",
-      globals: {
-        "ts-jest": {
-          isolatedModules: true,
-        },
-      },
+  return {
+    verbose: true,
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: ".coverage",
+    coverageProvider: "v8",
+    preset: "ts-jest",
 
-      testEnvironment: "jsdom",
-      maxWorkers: "50%",
-      // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-      //   prefix: "<rootDir>/",
-      // }),
+    testEnvironment: "jsdom",
+    maxWorkers: "50%",
+    // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    //   prefix: "<rootDir>/",
+    // }),
 
-      setupFilesAfterEnv: ["@iiko/jest-react-config-legacy/dist/setupAfterEnv.js"],
-      transform: {
-        "^.+\\.(ts|tsx|js|jsx)$": [
-          "ts-jest",
-          {
-            babelConfig: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
-              plugins: ["@babel/plugin-transform-modules-commonjs"],
-            },
+    ...options,
+    setupFilesAfterEnv: [
+      "@iiko/jest-react-config-legacy/dist/setupAfterEnv.js",
+      ...options.setupFilesAfterEnv,
+    ],
+
+    transformIgnorePatterns: [
+      "\\\\node_modules\\\\",
+      "\\.pnp\\.[^\\\\]+$",
+      ...(options.transformIgnorePatterns || []),
+    ],
+
+    transform: {
+      "^.+\\.(ts|tsx|js|jsx)$": [
+        "ts-jest",
+        {
+          babelConfig: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-transform-modules-commonjs"],
           },
-        ],
-        ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-          "jest-transform-stub",
-      },
-
-      transformIgnorePatterns: ["\\\\node_modules\\\\", "\\.pnp\\.[^\\\\]+$"],
+        },
+      ],
+      ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+        "jest-transform-stub",
+      ...options.transform,
     },
-    options
-  );
+
+    globals: {
+      ...options.globals,
+      "ts-jest": {
+        isolatedModules: true,
+        ...options.globals?.["ts-jest"],
+      },
+    },
+  };
 };
